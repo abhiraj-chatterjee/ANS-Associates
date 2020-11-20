171f938b657c19335ce68b9cc9966094d58e3cc0
abhiraj-chatterjee <abhirajchatterjee@gmail.com>
2020-11-20 19:33:52 +0530

    setup mongodb connection

--- api/api.py
+++ api/api.py
@@ -1,7 +1,17 @@
 import time
 from flask import Flask
+from flask_cors import CORS
+import pymongo
+from bson import ObjectId, json_util
+import json
 
 app = Flask(__name__)
+
+connectionURL = "mongodb+srv://abhiraj_chatterjee:paglaabu2001@ans-associates.2z78p.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority"
+client = pymongo.MongoClient(connectionURL)
+
+brochure = client.get_database("Brochure")
+# brochureCollection = brochure.brochureCollection
 
 @app.route('/time')
 def get_current_time():
